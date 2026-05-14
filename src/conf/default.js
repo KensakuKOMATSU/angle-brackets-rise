dc15dcf36276193401eaa2217e87de7e03a972d5
Kensaku Komatsu <kensaku.komatsu@gmail.com>
2026-05-14 13:01:13 +0900

    remove unneeded code from default.js

--- src/conf/default.js
+++ src/conf/default.js
@@ -1,16 +1,9 @@
+// set dmx map for your dmx device.
 const map = {
     1: "pan", 2: "tilt", 3: "dimmer", 4: "red", 5: "green", 6: "blue", 7: "white", 8: "speed"
 };
 
-// const map = {
-//     1: "red", 2: "green", 3: "blue", 4: "pan"
-// }
-
-export const colors = [
-    'rgb(0, 119, 204)',
-    'rgb(255, 127, 0)'
-]
-
+// set dmx configs for your dmx device.
 export const dmxConfigs = [
     { id: 0, name: "light0", start_address: 1, map },
     { id: 1, name: "light1", start_address: 10, map },
